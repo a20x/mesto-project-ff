6e5cf483be90b81acff63eb97110ab67f864223d
Aleksandr a20x <erm.alex03@gmail.com>
2024-04-03 15:28:11 +0300

    созданы и добавлены карточки на страницы. Написано их удаление по кнопке

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list');
+
+const cardTemplate = document.querySelector('#card-template').content;
+console.log('#card-template');
+
+function createCard(cardData) {
+  const cardTemplateContent = cardTemplate.querySelector('.card').cloneNode(true);
+
+  const cardImage = cardTemplateContent.querySelector('.card__image');
+  const cardTitle = cardTemplateContent.querySelector('.card__title');
+  const cardDeleteButton = cardTemplateContent.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  cardDeleteButton.addEventListener('click', function() {
+    deleteCard(cardTemplateContent);
+  })
+
+  return cardTemplateContent;
+}
+
+function deleteCard(elementToDelete) {
+  elementToDelete.remove();
+}
+
+initialCards.forEach(function(cardData) {
+  placesList.append(createCard(cardData));
+});
